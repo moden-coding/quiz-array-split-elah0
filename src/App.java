e0a74c589ecd9d9e2caa09e88091640b0b74a677
elah0 <28eleshem@gannacademy.org>
2024-11-14 13:38:24 -0500

    done

--- src/App.java
+++ src/App.java
@@ -39,8 +39,8 @@ public class App {
 
         //copyFrontAndBack example
         System.out.println("With input [1, 2, 3], you should get: [1, 3]. You got: " + Arrays.toString(copyFrontAndBack(new int[]{1,2,3})));
-        System.out.println("With input [1, 2, 3, 4], you should get: [1, 4]. You got: " + Arrays.toString(copyFrontAndBack(new int[]{1,2,3})));
-        System.out.println("With input [7], you should get: [7, 7]. You got: " + Arrays.toString(copyFrontAndBack(new int[]{1,2,3})));
+        System.out.println("With input [1, 2, 3, 4], you should get: [1, 4]. You got: " + Arrays.toString(copyFrontAndBack(new int[]{1,2,3,4})));
+        System.out.println("With input [7], you should get: [7, 7]. You got: " + Arrays.toString(copyFrontAndBack(new int[]{7})));
         
         //wordLength example
         System.out.println("With input \"Java is fun\" you should return 4. You got: " + maxWordLength("Java is fun"));
@@ -50,14 +50,33 @@ public class App {
 
     public static int[] copyFrontAndBack(int[] nums){
 
-        return null; //this is a placeholder
+        return new int[] { nums[0], nums[nums.length-1]};
 
     }
 
     public static int maxWordLength(String sentence){
+        String[] parts = sentence.split("");
+        String longestWord = "";
+        int longest =0;
+        String word ="";
+        int length =0;
+       
+        for (int i = 0; i < parts.length; i++) {
+             word = String.valueOf(parts[i]);
+            
+             
+            
+            
+        }
+        length = word.length();
+        if (length > longest){
+            longest = length;
+            longestWord = word;
+        }
+        
 
 
-        return -1; //this is a placeholder
+        return longestWord.length(); //this is a placeholder
     }
 
 
